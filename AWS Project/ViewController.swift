175cb199822e80dc01f5c023020648453c34888e
emin.emini <emin.emini@asynclabs.co>
2022-05-03 10:17:29 +0200

    Initial Commit

---
+++ AWS Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AWS Project
+//
+//  Created by Emin Emini on 03.05.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
