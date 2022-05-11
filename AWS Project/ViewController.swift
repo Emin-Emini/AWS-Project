a091cc2f0012bbe03aaacdc1fc7d7d0600f8f7f5
eminemini <eminrmae@gmail.com>
2022-05-11 13:10:59 +0200

    Setup AWS

--- AWS Project/ViewController.swift
+++ AWS Project/ViewController.swift
@@ -14,8 +14,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupAWSConfiguration()
     }
-
-
+    
+    
 }
 
+// MARK: - Configure AWS
+extension ViewController {
+    func setupAWSConfiguration() {
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:.EUCentral1,
+           identityPoolId:"eu-central-1:279bc153-1080-46a8-9bd1-eff2807579ee")
+
+        let configuration = AWSServiceConfiguration(region:.EUCentral1, credentialsProvider:credentialsProvider)
+
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+    }
+}
+
+// MARK: - Upload To AWS
+extension ViewController {
+    func uploadFile(with resource: String, type: String) {
+        let key = "\(resource).\(type)"
+    }
+}
